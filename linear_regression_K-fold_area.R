5fb20fc35aa0f4c74db28894785cedec399ca6e2
Albert Guo <sub.jl@icloud.com>
2020-11-25 02:28:44 +0000

    linear regression k-fold area & extent

--- linear_regression_K-fold_area.R
+++ linear_regression_K-fold_area.R
@@ -1,7 +1,7 @@
 library(MASS)
 library(tidyverse)
 
-arctic <- read.csv("091n.csv",stringsAsFactors = F) #load data
+arctic <- read.csv("arctic_data.csv",stringsAsFactors = F) #load data
 
 folds <- cut(seq(1,nrow(arctic)), breaks = 10, labels = FALSE) #data segmentation
 
@@ -15,7 +15,7 @@ prediction <- as.data.frame(
   # print(test) # if needed
   
   # linear regression
-  model <- lm(extent~rainfall+daylight+population+CO2+ozone+ocean_temp+land_temp,data=train)
+  model <- lm(area~rainfall+daylight+population+CO2+ozone+ocean_temp+land_temp,data=train)
   
   # print(summary(model)) # if needed
   
@@ -25,14 +25,14 @@ prediction <- as.data.frame(
 
 # table gathering and merging
 pred_gather <- gather(data=prediction, key="fold",value="prediction",1:10)
-result <- as.data.frame(cbind(arctic[,c(1,2,3,5)],pred_gather))
+result <- as.data.frame(cbind(arctic[,c(1,2,3,6)],pred_gather))
 
 # calculate value of R^2
-result["R^2"] <- ((result$extent-result$prediction)^2)
+result["R^2"] <- ((result$area-result$prediction)^2)
 R_square <- sum(result$`R^2`)/490
 
 # plot line chart (Prediction vs True)
-plot(result$observation,result$extent,type ='l',ylim = c(0,1.5),lwd = '2',
+plot(result$observation,result$area,type ='l',ylim = c(0,1.5),lwd = '2',
      xlab = "Observation_ID", ylab = "Value")
 lines(result$observation,result$prediction,lty=1,col='red',lwd = '2')
 
